c06bf6e02c50f59a2986902e2cd0030c5159ca02
Shyam Sundar <dssundar@hotmail.com>
2017-08-23 15:48:53 -0700

    zigzag for n 3 and above

--- leetcode/zigzag_conversion.py
+++ leetcode/zigzag_conversion.py
@@ -1,37 +1,45 @@
+def zigzag_conversion(input_string, n):
+    '''
+    Returns zig zag format of a string
+    :param input_string: string
+    :param n: int
+    :return: string
+    '''
+    i = 0
+    j = -2
+    flag = 1
+
+    temp_lists = [[] for _ in range(n)]
+
+    for letter in word:
+        if flag == 1:
+            temp_lists[i].append(letter)
+            i += 1
+
+            if i == n:
+                i = 0
+                flag = 0
+
+        else:
+            temp_lists[j].append(letter)
+            j -= 1
+
+            if j == -n:
+                j = -2
+                flag = 1
+
+    output_list = []
+
+    for l in temp_lists:
+        output_list.extend(l)
+
+    return ''.join(output_list)
+
+#-----------_TEST_--------------
 word = 'PAYPALISHIRING'
 n = 10
 
-i = 0
-j = -2
-flag = 1
+print(zigzag_conversion(word, n))
 
-temp_lists =[[] for _ in range(n)]
-
-
-for letter in word:
-    if flag == 1:
-        temp_lists[i].append(letter)
-        i += 1
-
-        if i == n:
-            i = 0
-            flag = 0
-
-    else:
-        temp_lists[j].append(letter)
-        j -= 1
-
-        if j == -n:
-            j = -2
-            flag = 1
-
-output_list = []
-
-
-
-for l in temp_lists:
-    output_list.extend(l)
-
-print(''.join(output_list))
 
 
