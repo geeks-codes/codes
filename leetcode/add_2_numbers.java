b4956fb9535cee18ee0f7f55e5a4d71b1c6c3fd5
geeks-codes <laashiha@gmail.com>
2017-09-05 22:39:13 -0700

    Create add_2_numbers.java

---
+++ leetcode/add_2_numbers.java
@@ -0,0 +1,63 @@
+2->4->3 + 5->6->4 = 7->0->8
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) 
+    {
+        ListNode temp1 = l1;
+        ListNode temp2 = l2;
+        int s = 0, carry = 0, a=0, b=0, data=0;
+        ListNode head = null;
+        ListNode prev = head;
+        
+        while(true)
+        {
+            if(temp1 == null && temp2 == null)
+            {
+                break;
+            }
+            else if(temp2 == null)
+            {
+                temp2 = new ListNode(0);
+                temp2.next = null;
+            }
+            else if(temp1 == null)
+            {
+                temp1 = new ListNode(0);
+                temp1.next = null;
+            }
+            a = temp1.val;
+            b = temp2.val;
+            s = a + b + carry;
+            carry = s/10;
+            data = s%10;
+            if(head == null)
+            {
+                head = new ListNode(data);
+                head.next = null;
+                prev = head;
+            }
+            else
+            {
+                prev.next = new ListNode(data);
+                prev.next.next = null;
+                prev = prev.next;
+            }
+            temp1 = temp1.next;
+            temp2 = temp2.next;
+        }
+        if(carry == 1)
+        {
+            prev.next = new ListNode(1);
+            prev.next.next = null;
+        }
+     return head;   
+    }
+}
