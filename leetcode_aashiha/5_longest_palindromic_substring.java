a5dedcaab6ae4c3dbc8318be92e1a54b32605a61
geeks-codes <laashiha@gmail.com>
2017-09-28 12:17:53 -0700

    Update 5_longest_palindromic_substring.java

--- leetcode_aashiha/5_longest_palindromic_substring.java
+++ leetcode_aashiha/5_longest_palindromic_substring.java
@@ -1,35 +1,28 @@
 import java.io.*;
 class Solution 
 {
-    public boolean isPalindrome(String s1)
-    {
-        String s2 = new StringBuffer(s1).reverse().toString();
-        //System.out.println("\n is palindrome "+s1+"  "+s2);
-        return s1.equals(s2);
-    }
+    public int low=0, max_len=0;
     public String longestPalindrome(String s) 
     {
-        int len = s.length();
-        char[] arr = s.toCharArray();
-        int max_len = 0;
-        String max_str = "";
-        for(int i=0;i<len;i++)
+        int n = s.length();
+        for(int i=0;i<n;i++)
         {
-            int j=i;
-            while(j<=len)
-            {
-                String s1 = s.substring(i,j);
-                if(isPalindrome(s1))
-                {
-                    if(s1.length() > max_len)
-                    {
-                        max_len = s1.length();
-                        max_str = s1;
-                    }
-                }
-                j++;
-            }
+            check_palindrome(s,i,i);
+            check_palindrome(s,i,i+1);
         }
-        return max_str;
+        return s.substring(low, max_len+low);
+    }
+    public void check_palindrome(String s, int i,int j)
+    {
+        while(i>=0 && j<s.length() && s.charAt(i)==s.charAt(j))
+        {
+            i--;
+            j++;
+        }
+        if(max_len < j-i-1)
+        {
+            max_len = j-i-1;
+            low = i+1;
+        }
     }
 }
